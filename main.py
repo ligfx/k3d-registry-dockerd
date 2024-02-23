79c1b6810449b0bee0d800ac4769a5323f793c44
Michael Maltese <michaeljosephmaltese@gmail.com>
2024-02-23 15:31:25 -0500

    use docker local cache on filesystem if we can find it

--- main.py
+++ main.py
@@ -1,37 +1,25 @@
-from flask import Flask, request
 import docker
-import tarfile
-import json
+import glob
 import hashlib
+import tarfile
+from flask import Flask, request, send_file
 
-def to_file_like_obj(iterable):
-    chunk = b''
-    offset = 0
-    it = iter(iterable)
+def docker_get_image_tarball(image_id):
+    # get the raw response since Docker SDK turns it into an iterator
+    res = client.api._get(client.api._url("/images/{0}/get", image_id), stream=True)
+    client.api._raise_for_status(res)
+    socket = client.api._get_raw_response_socket(res)
+    client.api._disable_socket_timeout(socket)
+    res.raw.decode_content = True
 
-    def up_to_iter(size):
-        nonlocal chunk, offset
-
-        while size:
-            if offset == len(chunk):
-                try:
-                    chunk = next(it)
-                except StopIteration:
-                    break
-                else:
-                    offset = 0
-            to_yield = min(size, len(chunk) - offset)
-            offset = offset + to_yield
-            size -= to_yield
-            yield chunk[offset - to_yield:offset]
-
-    class FileLikeObj:
-        def __init__(self):
+    class ForwardSeekableWrapper:
+        def __init__(self, f):
+            self._f = f
             self._p = 0
         def tell(self):
             return self._p
-        def read(self, size=-1):
-            result =  b''.join(up_to_iter(float('inf') if size is None or size < 0 else size))
+        def read(self, size=None):
+            result = self._f.read(size)
             self._p += len(result)
             return result
         def seek(self, where):
@@ -39,8 +27,8 @@ def to_file_like_obj(iterable):
                 self.read(where - self._p)
                 return
             raise NotImplementedError(self._p, where)
-
-    return FileLikeObj()
+    
+    return ForwardSeekableWrapper(res.raw)
 
 app = Flask(__name__)
 client = docker.from_env()
@@ -54,27 +42,38 @@ def v2():
     # return ('', 204)
     return "Hello, world!"
 
-def find_blob(domain, namespace, image, reference):
+def find_blob(domain, namespace, image, reference, mimetype):
     if not domain:
         # don't support acting as own repo
-        return
+        return ('', 404)
     
     if not reference.startswith('sha256:'):
         raise NotImplementedError("Bad reference %r" % reference)
     
+    # pull from Docker's export cache, if it exists
+    # TODO: pull from Docker's containerd storage (overlayfs?), if that storage engine is being used
+    cached = glob.glob(f'/var/lib/docker/tmp/docker-export-*/blobs/sha256/{reference.split("sha256:")[1]}')
+    if cached:
+        app.logger.info(f'using cached file {cached[0]}')
+        return send_file(cached[0], mimetype=mimetype if mimetype else 'application/octet-stream')
+
+    # go pull some tarballs and take a look
     name=f'{domain}/{namespace}/{image}'
     if name.startswith('docker.io/'):
         name = name[len('docker.io/'):]
     for image_obj in client.images.list(name):
         # TODO: could probably be smarter and keep a cache of which blobs belong to which images?
-        t = tarfile.TarFile(fileobj=to_file_like_obj(image_obj.save()))
+        t = tarfile.TarFile(fileobj=docker_get_image_tarball(image_obj.id))
         while True:
             ti = t.next()
             if ti is None:
                 break
             if ti.name == f'blobs/sha256/{reference.split("sha256:")[1]}':
-                fi = t.extractfile(ti)
-                return fi.read()
+                # TODO: could also get mimetype by trying blobs that look like JSON / come from
+                # a manifest request, and use the embedded mediaType
+                resp = send_file(t.extractfile(ti), mimetype=mimetype if mimetype else 'application/octet-stream')
+                resp.headers.add('content-length', ti.size)
+                return resp
             if ti.name == 'index.json':
                 fi = t.extractfile(ti)
                 bdata = fi.read()
@@ -82,26 +81,32 @@ def find_blob(domain, namespace, image, reference):
                 m.update(bdata)
                 app.logger.info("trying index.json, digest %r" % m.hexdigest())
                 if m.hexdigest() == reference.split("sha256:")[1]:
-                    return bdata
+                    return (bdata, 200, {"content-type": "application/vnd.oci.image.index.v1+json"})
+                
+    return ('No blob found', 404)
 
 @app.route("/v2/<namespace>/<image>/blobs/<reference>")
 def blobs(namespace, image, reference):
-    domain = request.args.get('ns')
-
     if request.method != 'GET':
         raise NotImplementedError()
+
+    domain = request.args.get('ns')
+    app.logger.info(repr(request.headers))
 
     if not domain:
         # don't support acting as own repo
         return ("Requires a 'ns' query param", 500)
 
-    blob = find_blob(domain, namespace, image, reference)
-    if blob is None:
-        return ('No blob found', 404)
-    return (blob, 200)
+    mimetype = request.headers.get('accept').split(',')[0].strip()
+    return find_blob(domain, namespace, image, reference, mimetype=mimetype)
 
 @app.route("/v2/<namespace>/<image>/manifests/<reference>")
 def manifests(namespace, image, reference):
+    if request.method not in ('HEAD', 'GET'):
+        raise ('', 405)
+
+    app.logger.info(repr(request.headers))
+
     domain = request.args.get('ns')
     if not domain:
         # don't support acting as own repo
@@ -110,10 +115,8 @@ def manifests(namespace, image, reference):
     # TODO: check accept header
 
     if reference.startswith('sha256:'):
-        blob = find_blob(domain, namespace, image, reference)
-        if blob is None:
-            return ('No manifest found', 404)
-        return (blob, 200)
+        mimetype = request.headers.get('accept').split(',')[0].strip()
+        return find_blob(domain, namespace, image, reference, mimetype=mimetype)
 
     def find_image():
         name=f'{domain}/{namespace}/{image}'
@@ -131,42 +134,23 @@ def manifests(namespace, image, reference):
     if not image_obj:
         return ('', 404)
 
-    app.logger.info(repr(request.headers))
-
     if request.method == 'HEAD':
         return ('', 200)
-    if request.method != 'GET':
-        raise NotImplementedError()
 
-    t = tarfile.TarFile(fileobj=to_file_like_obj(image_obj.save()))
+    # this tarfile is in OCI image format
+    t = tarfile.TarFile(fileobj=docker_get_image_tarball(image_obj.id))
     while True:
         ti = t.next()
         if ti is None:
             break
-        app.logger.info(ti.name)
+        app.logger.info(ti)
         if ti.name == 'index.json':
-            fi = t.extractfile(ti)
-            bdata = fi.read()
+            bdata = t.extractfile(ti).read()
             m = hashlib.sha256()
             m.update(bdata)
             digest = m.hexdigest()
             app.logger.info(digest)
             return (bdata, 200, {"content-type": "application/vnd.oci.image.index.v1+json"})
-
-        # fi = t.extractfile(ti)
-        # if fi:
-        #     bdata = fi.read(1024)
-        #     if b'application/vnd.oci.image.manifest.v1+json' in bdata:
-        #         bdata = bdata + fi.read()
-        #         data = json.loads(bdata.decode('utf-8'))
-        #         if data.get('mediaType') == 'application/vnd.oci.image.manifest.v1+json':
-        #             app.logger.info(data)
-        #             m = hashlib.sha256()
-        #             m.update(bdata)
-        #             m.hexdigest()
-        #             # TODO: should we be returning the header
-        #             g_manifests_seen[f'{namespace}/{image}/sha256:{m.hexdigest()}'] = bdata
-        #             return (bdata, 200, {"content-type": 'application/vnd.oci.image.manifest.v1+json', 'Docker-Content-Digest': m.hexdigest()})
     return ('No manifest found', 500)
 
 if __name__ == "__main__":
--- run_image.sh
+++ run_image.sh
@@ -9,9 +9,12 @@ docker container rm -f my-registry || true
 docker network rm k3d-mytest || true
 
 docker network create k3d-mytest
-docker run -p 49156:5000 --name my-registry --network k3d-mytest -d -v /var/run/docker.sock:/var/run/docker.sock ligfx/my-registry-shim 
+docker run -p 49156:5000 --name my-registry --network k3d-mytest -d -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker ligfx/my-registry-shim
 
-k3d cluster create mytest --registry-config registry.yml --network k3d-mytest
+k3d cluster create mytest --registry-config registry.yml --network k3d-mytest \
+    --servers "1" --servers-memory "1.5g" \
+    --agents "3" --agents-memory "4g" \
+    -i 'docker.io/rancher/k3s:v1.27.11-rc1-k3s1'
 # kubectl run test --image ligfx/my-other-image 
 
 docker logs -f my-registry
