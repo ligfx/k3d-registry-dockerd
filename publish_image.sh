61fb43ffb14d4bd30aba278332c047d8349987c1
Michael Maltese <michaeljosephmaltese@gmail.com>
2024-02-27 14:47:12 -0500

    update publish_image.sh to push tags

--- publish_image.sh
+++ publish_image.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
-set -euxo pipefail
+set -euo pipefail
 
-docker buildx build . -t ligfx/k3d-registry-dockerd --platform linux/arm64,linux/amd64 --push
+versions=$(git tag --points-at HEAD | grep '^v')
+if test "$(git rev-parse --abbrev-ref HEAD)" = "main"; then
+    versions="$versions latest"
+fi
+if test -z "$versions"; then
+    echo >&2 "Don't know which versions to push!"
+    exit 1
+fi
+echo "Will publish: $versions"
+image_tags=""
+for ver in $versions; do
+    image_tags="${image_tags} -t ligfx/k3d-registry-dockerd:$ver"
+done
+docker buildx build . $image_tags --platform linux/arm64,linux/amd64 --push
