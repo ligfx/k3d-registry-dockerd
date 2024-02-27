278db579344630c164a1034f16abb2d11d2f255d
Michael Maltese <michaeljosephmaltese@gmail.com>
2024-02-27 12:14:32 -0500

    update image name

--- run_image.sh
+++ run_image.sh
@@ -2,14 +2,14 @@
 
 set -euxo pipefail
 
-docker build . -t ligfx/my-registry-shim
+docker build . -t ligfx/k3d-registry-dockerd
 
 k3d cluster delete mytest || true
 docker container rm -f my-registry || true
 docker network rm k3d-mytest || true
 
 docker network create k3d-mytest
-docker run -p 49156:5000 --name my-registry --network k3d-mytest -d -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker ligfx/my-registry-shim
+docker run -p 49156:5000 --name my-registry --network k3d-mytest -d -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker ligfx/k3d-registry-dockerd
 
 k3d cluster create mytest --registry-config registry.yml --network k3d-mytest \
     --servers "1" --servers-memory "1.5g" \
