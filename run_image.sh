2880e7c03b2092cc569a358aa12e3c7a60a32bc0
Michael Maltese <michaeljosephmaltese@gmail.com>
2024-02-27 13:52:16 -0500

    run_image.sh: use config file to have k3d spin up registry

--- run_image.sh
+++ run_image.sh
@@ -5,16 +5,20 @@ set -euxo pipefail
 docker build . -t ligfx/k3d-registry-dockerd
 
 k3d cluster delete mytest || true
-docker container rm -f my-registry || true
-docker network rm k3d-mytest || true
 
-docker network create k3d-mytest
-docker run -p 49156:5000 --name my-registry --network k3d-mytest -d -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker ligfx/k3d-registry-dockerd
+configfile=$(mktemp)
+cat << HERE > "$configfile"
+apiVersion: k3d.io/v1alpha5
+kind: Simple
+registries:
+  create:
+    image: ligfx/k3d-registry-dockerd
+    proxy:
+      remoteURL: "*"
+    volumes:
+      - /var/run/docker.sock:/var/run/docker.sock
+      - /var/lib/docker:/var/lib/docker
+HERE
+k3d cluster create mytest --config "$configfile"
 
-k3d cluster create mytest --registry-config registry.yml --network k3d-mytest \
-    --servers "1" --servers-memory "1.5g" \
-    --agents "3" --agents-memory "4g" \
-    -i 'docker.io/rancher/k3s:v1.27.11-rc1-k3s1'
-# kubectl run test --image ligfx/my-other-image 
-
-docker logs -f my-registry
+docker logs -f k3d-mytest-registry
