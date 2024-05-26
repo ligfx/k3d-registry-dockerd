08ce06495a9f1239ebbc0fe313639f3d488a8236
Michael Maltese <michaeljosephmaltese@gmail.com>
2024-05-25 22:48:28 -0400

    publish_image.sh: check that working directory matches what's in git

--- publish_image.sh
+++ publish_image.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
+if ! git diff-index --quiet HEAD --; then
+    echo >&2 "Can't build and publish images from a dirty working directory!"
+    exit 1
+fi
+
 versions=$(git tag --points-at HEAD | grep '^v')
 if test "$(git rev-parse --abbrev-ref HEAD)" = "main"; then
     versions="$versions latest"
